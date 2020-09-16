e3f021dff974e94990078d7fd09cc1e2f3f20570
Michael Beckwith <tw2113@gmail.com>
2020-09-16 01:33:21 -0500

    barebone starting for header and footer files

--- themes/digitalgarden/footer.php
+++ themes/digitalgarden/footer.php
@@ -1,1 +1,4 @@
-<?php
+<?php wp_footer(); ?>
+
+</body>
+</html>
--- themes/digitalgarden/header.php
+++ themes/digitalgarden/header.php
@@ -1,1 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<link rel="profile" href="http://microformats.org/profile/specs" />
+	<link rel="profile" href="http://microformats.org/profile/hatom" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
