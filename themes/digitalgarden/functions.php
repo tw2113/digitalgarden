52f1b53d43b73b83ef67ea3b31f36c7f03a112a8
Michael Beckwith <tw2113@gmail.com>
2020-09-17 02:08:28 -0500

    load more files

--- themes/digitalgarden/functions.php
+++ themes/digitalgarden/functions.php
@@ -1,1 +1,9 @@
 <?php
+
+namespace digitalgarden;
+
+function load_inc() {
+	require_once get_template_directory() . '/inc/query-filters.php';
+	require_once get_template_directory() . '/inc/content-type-edits.php';
+}
+add_action( 'after_setup_theme', __NAMESPACE__ . '\load_inc' );
