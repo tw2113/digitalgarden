05d08769ea13824654ed96a6081faa16beab4d59
Michael Beckwith <tw2113@gmail.com>
2020-09-22 00:24:38 -0500

    temp title for the momnet

--- themes/digitalgarden/header.php
+++ themes/digitalgarden/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <title>Digital Garden</title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
 	<link rel="profile" href="http://microformats.org/profile/specs" />
