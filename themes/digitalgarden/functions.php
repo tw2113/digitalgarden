e1e275d7b86192714d420c5cea9402359779f80b
Michael Beckwith <tw2113@gmail.com>
2021-02-06 17:42:14 -0600

    favicon

--- themes/digitalgarden/functions.php
+++ themes/digitalgarden/functions.php
@@ -39,3 +39,11 @@ function webmention_callback( $comment, $args, $depth ) {
 	</div>
 <?php
 }
+
+function favicon() {
+	?>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧤</text></svg>">
+	<?php
+}
+add_action( 'wp_head', __NAMESPACE__ . '\favicon' );
+add_action( 'admin_head', __NAMESPACE__ . '\favicon' );
